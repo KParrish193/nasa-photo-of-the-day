e87f982edf85f69120c9cbf028354bd14a41d13e
Kristi Parrish <kp@Kristis-MBP.hsd1.ut.comcast.net>
2019-12-12 22:20:48 -0700

    Popup added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Photo from "./Components/Photo";
 import axios from "axios";
 import "./App.css";
 
-function App() {
+import {Button, InputGroup, InputGroupAddon, Input} from 'reactstrap';
 
-  const [photoData, setPhotoData] = useState([]);
+
+function App(){
+
+  const [photoData, setPhotoData] = useState({});
+  const [date, setDate] = useState({});
 
     useEffect(() => {
       axios
         .get(`https://api.nasa.gov/planetary/apod?api_key=rLpVkzWLLfhepK1qkqpWYw7hsj8x1wNk73mHUmGH`)
         .then(response => {
           console.log(response.data)
-          setPhotoData([response.data]);
+          setPhotoData(response.data);
         })
         .catch(error => {
         console.log("The data was not returned, error");
@@ -26,18 +30,22 @@ function App() {
               <img src="../nasalogo.png" alt="nasa logo"></img>
             </div>
             <h1>Astronomy Photo of the Day</h1>
-          </div>
+              <div className="buttons">
+              <Button onClick={() => setDate(date + 1)}>&#10094;  Next Day {}</Button> 
+              <Button onClick={() => setDate(date)}>Today:  {photoData.date}</Button>
+              <Button onClick={() => setDate(date - 1)}> Previous Day &#10095;</Button>
+            </div>
+            </div>
+
 
         <div className="photocard">
-          {photoData.map((item, index) => {
-            return <Photo key={index} 
-            title={item.title}
-            date={item.date}
-            explanation={item.explanation}
-            image={item.hdurl} />
-          })}
+            <Photo
+            title={photoData.title}
+            date={photoData.date}
+            image={photoData.hdurl} 
+            photog={photoData.copyright}
+            explanation={photoData.explanation} />
         </div>
-
     </div>
   );
 };
--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import "./../App.css";
+import {Card, CardImg, CardBody, CardText, Button, UncontrolledPopover, PopoverBody, PopoverHeader} from 'reactstrap';
 
 const Photo = props => {
     console.log(props);
+
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const toggle = () => setPopoverOpen(!popoverOpen);
+
     return (
-      
-      <div className="photo-card">
-        <h2>{props.title}</h2>
-        <p>Date: {props.date} </p>
-        <div className="image">
-        <img src={props.image} alt="space"/>
-        </div>
-        <p>{props.explanation}</p>
-        
+      <div>
+      <div>
+        <Card className="photo-card">
+        <CardBody>
+          <Button id="Popover1" type="button">
+            Details
+          </Button><CardText>Date: {props.date} Photo Credit: {props.photog}</CardText>
+          <CardImg src={props.image} alt="space"/>
+          </CardBody>
+        </Card>
+      </div>
+      <div>
+        <UncontrolledPopover trigger="click" placement="bottom"  target="Popover1" isOpen={popoverOpen} toggle={toggle}>
+            <PopoverHeader>
+              {props.title}
+            </PopoverHeader>
+            <PopoverHeader>
+              Date: {props.date} / Credit: {props.photog}
+            </PopoverHeader>
+            <PopoverBody>
+              {props.explanation}
+            </PopoverBody>
+        </UncontrolledPopover>
+      </div>
       </div>
     );
   };
